f1d156d79f794b72793cfdee6a91294dcb5aa3d5
whoisarjen <kamilow97@gmail.com>
2022-03-01 11:36:15 +0100

    v

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './main.css';
 import './i18n';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-GKGD78CS99');
 
 ReactDOM.render(
     <React.StrictMode>
